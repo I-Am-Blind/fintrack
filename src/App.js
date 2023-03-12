e5cdbb5184ee61c3c605097c9e54ecafb3a7f0cd
Amjad Ali <cowboyamjad@gmail.com>
2023-03-12 14:40:07 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           <img src={pro} className="h-6 w-6" />
           <h3> Welcome Adarsh </h3>
         </div>
-        <div className="flex gap-16">
+        <div className="flex gap-8">
           <div>Home</div>
           <div>Settings</div>
           <div>Logout</div>
@@ -18,11 +18,11 @@ function App() {
       </div>
 
       <div className="container w-screen h-full flex gap-4 ">
-        <div className="left-container  h-[30rem] w-[60rem] rounded-xl  border-[1px] border-solid">
+        <div className="left-container  h-[30rem] w-[60rem] rounded-xl  border-[1px] border-solid shadow-3xl">
           <div className="text-xl font-semibold font-primary text-white p-4">
             Recent Transactions
           </div>
-          <div className="card-container flex gap-2 justify-center items-center px-4">
+          <div className="card-container flex gap-2 justify-start items-center px-4">
             <Card name="Spotify" plan="individual plan" price="500" />
             <Card name="Spotify" plan="individual plan" price="500" />
             <Card name="Spotify" plan="individual plan" price="500" />
@@ -34,22 +34,32 @@ function App() {
           </div>
 
           <div className="t-container px-4">
-            <div className="container-2 flex gap-8">
-             <div>
-             <input
-                className="h-8 w-24 rounded-xl  px-2"
-                placeholder="Name"
-              ></input>
+            <div className="container-2 flex gap-16">
+              <div className="flex gap-2">
+                <input
+                  className="h-8 w-24 rounded-xl  px-2"
+                  placeholder="Name"
+                ></input>
 
-<input
-                className="h-8 w-24 rounded-xl  px-2"
-                placeholder="Amount"
-              ></input>
-             </div>
+                <input
+                  className="h-8 w-24 rounded-xl  px-2"
+                  placeholder="Amount"
+                ></input>
+              </div>
 
               <button className="button rounded-xl p-2 text-white font-primary text-sm shadow-xl">
                 Add transaction
               </button>
+            </div>
+            <div className="flex justify-between items-center">
+            <div className="text-white font-primary mt-4 opacity-70 font-light">
+              <div className="flex "><h2> Bakery :</h2><h2>₹ 200</h2></div>
+              <div className="flex "><h2> Grocery :</h2><h2>₹ 500</h2></div>
+              <div className="flex "><h2> Games :</h2><h2>₹ 3542</h2></div>
+              <div className="flex "><h2> Clothes :</h2><h2>₹ 5600</h2></div>
+              <div className="flex"><h2> Tuition fee :</h2><h2>₹ 25000</h2></div>
+            </div>
+            <div className="text-xl text-white font-primary mr-8 p-2 border-[1px]  border-solid rounded-xl">Total : ₹38400</div>
             </div>
           </div>
         </div>
