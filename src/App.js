b0ecd7a3d4a23cef571d668a34cfcd4c58a42ef1
Amjad Ali <cowboyamjad@gmail.com>
2023-03-12 12:53:16 +0530

    setup pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import bg from './bg.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen flex ">
+     <img className='w-screen h-screen absolute' src={bg}/>
+     <div className='navbar bg-white h-32 w-screen'></div>
     </div>
   );
 }
