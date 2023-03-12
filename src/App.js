4c2762c82a53d59c8737576f8aed2e6d66f2eb40
Amjad Ali <cowboyamjad@gmail.com>
2023-03-12 13:14:47 +0530

    adde fonts

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="h-screen w-screen flex ">
-     <img className='w-screen h-screen absolute' src={bg}/>
-     <div className='navbar bg-white h-32 w-screen'></div>
+    <div className="h-screen w-screen bg-bgimage bg-no-repeat bg-cover bg-center bg-fixed flex flex-col">
+
+      <div className='navbar w-screen h-16 text-white font-primary'>
+        <div>Profile</div>
+        <div>
+          <div>Home</div>
+          <div>Settings</div>
+          <div>Logout</div>
+        </div>
+      </div>
+      <div className='container w-screen h-full'></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bgimage': "url('bg.png')",
+      },
+      fontFamily: {
+        'primary': ['Space Grotesk', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
